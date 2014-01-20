93c90b0f61c0f335bbce2bcca8c3b4a60110f197
Yuriy Luchaninov <yluchaninov@gmail.com>
2014-01-20 13:55:54 +0200

    scrollbar refactoring

---
+++ scrollbar/scrollbar.js
@@ -0,0 +1,41 @@
+function ScrollBar(wrappable, className) {
+    var mPrevFunc = wrappable.setPosition,
+        mPrevRefresh = wrappable.refresh,
+        mContainer = wrappable.container,
+        mBar = document.createElement('div'),
+        mLastPosition,
+        mHeight,
+        mTransformName,
+        mTranslateArray = [],
+        mPXStr = "px";
+
+    function updateBar() {
+        mBar.style.height = mContainer.offsetHeight * (mContainer.offsetHeight / (wrappable.getMaxPosition() + mContainer.offsetHeight)) + mPXStr;
+        mHeight = mContainer.offsetHeight - mBar.offsetHeight;
+    }
+
+    wrappable.setPosition = function () {
+        mPrevFunc.apply(this, arguments);
+
+        mLastPosition = -wrappable.scrollPosition * mHeight / wrappable.getMaxPosition();
+        mTranslateArray[1] = mLastPosition;
+        mBar.style[mTransformName] = mTranslateArray.join("");
+    };
+
+    wrappable.refresh = function () {
+        mPrevRefresh.apply(this, arguments);
+        updateBar();
+    };
+
+    mTransformName = addVendorPrefix("transform");
+    mTranslateArray[0] = "translate3d(0, ";
+    mTranslateArray[2] = "px, 0) scale(1)";
+
+    mBar.style.position = "absolute";
+    mBar.className = className;
+    mContainer.appendChild(mBar);
+
+    updateBar();
+
+    return wrappable;
+}
